1488262d0d5099828f572227b0b567c71198ffd1
Lucas Santos <lucassantos10@gmail.com>
2025-01-21 17:24:13 -0300

    run build

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.listen(PORT, HOSTNAME, async (error?: Error) => {
             createAssociationsModels();
             await sequelize.sync({ alter: true });
             await IfesController.createIfesByIfesJsonList();
-            console.log('Conexao com banco de dados realizada com sucesso!');
+            console.log('Conexao com banco de dados realizada com sucesso.');
             console.log(`Servidor online no endereco: http://${HOSTNAME}:${PORT}`);
 
         } catch (error) {
