308de088d9a29610a82148201c672f560b5efb7f
luizfcneto <luizfcneto123@gmail.com>
2025-02-07 20:34:34 -0300

    +Adicionando erros novos na camada de validacao

--- src/modules/api/validations/DataValidations.ts
+++ src/modules/api/validations/DataValidations.ts
@@ -1,3 +1,5 @@
+import BadRequestError from "../../../errors/BadRequestError";
+
 export default class DataValidations {
 
     /**
@@ -7,7 +9,7 @@ export default class DataValidations {
     static validateData(data: string) {
         const regexData = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/([1-9]\d{3})$/;
         if (!regexData.test(data)) {
-            throw new Error(`Erro ao validar o campo data ${data}`);
+            throw new BadRequestError(`Erro ao validar o campo data ${data}`);
         }
     }
 }
